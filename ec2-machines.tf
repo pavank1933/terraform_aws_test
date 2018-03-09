4f6e33be119cc497d204413192be8c3eed6613f4
pavan kumar <pavandevops33@gmail.com>
2018-03-09 23:50:00 +0530

    readme updaed

--- ec2-machines.tf
+++ ec2-machines.tf
@@ -14,14 +14,6 @@ resource "aws_security_group" "elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
   # outbound internet access
   egress {
     from_port   = 0
