a8bdf61a8dfd9e36c4078e520d4a99e5182460d8
pavan kumar <pavandevops33@gmail.com>
2018-03-11 21:42:51 +0530

    add file

--- ec2-machines.tf
+++ ec2-machines.tf
@@ -154,7 +154,7 @@ resource "aws_instance" "prometheus_server" {
   }  
   user_data = <<HEREDOC
   #!/bin/bash
-  sleep 180  
+  #sleep 180  
   wget https://github.com/prometheus/prometheus/releases/download/v2.2.0/prometheus-2.2.0.linux-amd64.tar.gz
   tar xvfz prometheus-*.tar.gz
   cd prometheus-*
