d997ada9e97627c5c86ee580a5cbdf97ed19b64a
pavan kumar <pavandevops33@gmail.com>
2018-03-11 21:56:53 +0530

    updated ec2machines

--- ec2-machines.tf
+++ ec2-machines.tf
@@ -159,11 +159,11 @@ resource "aws_instance" "prometheus_server" {
   tar xvfz prometheus-*.tar.gz
   cd prometheus-*
   cp -rpf prometheus.yml  prometheus.yml-orig
-  #inst1= "${aws_instance.phpapp1.id}"
-  #inst2= "${aws_instance.phpapp2.id}"
-  #inst3= "${aws_instance.phpapp3.id}"
-  #echo "      - targets: ['"${aws_instance.phpapp1.id}":9100', '"${aws_instance.phpapp1.id}":9100', '"${aws_instance.phpapp1.id}":9100']" >> /prometheus.yml
-  #echo "      labels" >> /prometheus.yml
-  #echo "        group: 'production' ">> /prometheus.yml
+  inst1= "${aws_instance.phpapp1.id}"
+  inst2= "${aws_instance.phpapp2.id}"
+  inst3= "${aws_instance.phpapp3.id}"
+  echo "      - targets: ['"${aws_instance.phpapp1.id}":9100', '"${aws_instance.phpapp1.id}":9100', '"${aws_instance.phpapp1.id}":9100']" >> /prometheus.yml
+  echo "      labels" >> /prometheus.yml
+  echo "        group: 'production' ">> /prometheus.yml
 HEREDOC
 }
